4a7793fa2bb358955de09988d351a350400d0758
MatsekolengHope <32402546+MatsekolengHope@users.noreply.github.com>
2018-08-29 16:08:54 +0200

    Add files via upload

---
+++ EOH/src/main/java/za/co/digitalplatoon/invoiceservice/invoice/InvoiceController.java
@@ -0,0 +1,36 @@
+package za.co.digitalplatoon.invoiceservice.invoice;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/invoices")
+public class InvoiceController {
+
+	@Autowired
+	private InvoiceRepository invoiceRepository;
+	
+	@PostMapping("/add-invoice")
+	public Invoice addInvoice(@RequestBody Invoice invoice){
+		invoice.setClient("Hope");
+		invoice.setVatRate((long) 14);
+		invoice.setInvoiceDate(new Date());
+		return invoiceRepository.save(invoice);
+	}
+	
+
+	@GetMapping("/all-invoices")
+	public List<Invoice> viewAllInvoices(){
+
+		return invoiceRepository.findAll();
+	}
+	
+	@RequestMapping("/get-one/{invoiceId}")
+	public Invoice viewInvoice(@PathVariable Long invoiceId){
+		return invoiceRepository.getOne(invoiceId);
+	}
+	
+}
